c5dbb0c7187a5e9a92b257bc1f392109f2cbf8a6
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-04-26 21:35:34 +0530

    scroll js animations added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 const header = document.querySelector("header");
 const scrollBtn = document.getElementById("scroll-btn");
+const menu = document.getElementById("menu-icon");
+const navlist = document.querySelector(".nav-list");
+
+// scroll on page behaviour
+const scrollFunction = () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+const topFunction = () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
 
 // handle scroll event
 window.addEventListener("scroll", () => {
@@ -7,26 +24,39 @@ window.addEventListener("scroll", () => {
 });
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navlist.classList.remove("open");
   scrollFunction();
 };
 
 // hide scroll on first page load
-window.onload = function () {
+window.onload = () => {
   scrollFunction();
 };
 
-// scroll on page behaviour
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    scrollBtn.style.display = "block";
-  } else {
-    scrollBtn.style.display = "none";
-  }
-}
+// Handle menu click
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+};
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+// Scroll reveal animations
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "85px",
+  duration: 2500,
+  reset: true,
+});
+
+sr.reveal(".home-text", { delay: 300 });
+sr.reveal(".home-img", { delay: 400 });
+sr.reveal(".container", { delay: 400 });
+
+sr.reveal(".about-img", {});
+sr.reveal(".about-text", { delay: 300 });
+
+sr.reveal(".middle-text", {});
+sr.reveal(".shop-content,.row-btn", { delay: 300 });
+
+sr.reveal(".review-content,.contact", { delay: 300 });
