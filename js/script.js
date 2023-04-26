b5ed46c5d50d2fd4e6a8436b75fa8f6bdfc6da29
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-04-26 20:53:59 +0530

    scroll to top button created

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
 const header = document.querySelector("header");
+const scrollBtn = document.getElementById("scroll-btn");
 
+// handle scroll event
 window.addEventListener("scroll", () => {
   header.classList.toggle("sticky", window.scrollY > 80);
 });
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+// hide scroll on first page load
+window.onload = function () {
+  scrollFunction();
+};
+
+// scroll on page behaviour
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
