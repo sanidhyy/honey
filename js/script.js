f6b4a049010907c55a0429a4f92667da85ba8d2a
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-04-28 20:33:03 +0530

    nav active element

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
+// get elements
 const header = document.querySelector("header");
 const scrollBtn = document.getElementById("scroll-btn");
 const menu = document.getElementById("menu-icon");
 const navlist = document.querySelector(".nav-list");
+const navlistEl = document.querySelectorAll(".nav-list li a");
+
+// handle nav list element on click
+navlist.addEventListener("click", (e) => {
+  // remove active class from every element
+  navlistEl.forEach((el) => {
+    if (el.classList.contains("active")) el.classList.remove("active");
+  });
+
+  // add active class to current element
+  if (e.target.tagName === "A") {
+    if (!e.target.classList.contains("active"))
+      e.target.classList.add("active");
+  }
+});
 
 // scroll on page behaviour
 const scrollFunction = () => {
@@ -49,6 +65,7 @@ const sr = ScrollReveal({
   reset: true,
 });
 
+// animations
 sr.reveal(".home-text", { delay: 300 });
 sr.reveal(".home-img", { delay: 400 });
 sr.reveal(".container", { delay: 400 });
